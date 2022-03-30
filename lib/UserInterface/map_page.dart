39ae44d3f809c83cfe607b6e842df5bee2ec96e3
Theolin Naidoo <Theolin.Naidoo@standardbank.co.za>
2022-03-30 21:20:45 +0200

    - took out mapbox garbage - Gooogle Maps Works :D

--- lib/UserInterface/map_page.dart
+++ lib/UserInterface/map_page.dart
@@ -1,57 +1,59 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() {
-  runApp(const MapPage());
-}
+void main() => runApp(MapPage());
 
 class MapPage extends StatelessWidget {
-  const MapPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        body: FlutterMap(
-          options: MapOptions(
-            center: LatLng(-25.783602670969653, 28.335792636819498),
-            //-25.783602670969653, 28.335792636819498
-            zoom: 13.0,
-          ),
-          layers: [
-            TileLayerOptions(
-              urlTemplate: "https://api.mapbox.com/styles/v1/theolinnaidoo/cl17xbsf7005015lj5oh12ezl/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGhlb2xpbm5haWRvbyIsImEiOiJjbDE3djd6ejcwN3FsM2NvNGdmMGFwZzN3In0.cKJRbP39WngD5JNwsphF-g",
-              additionalOptions: {
-                'accessToken' : 'pk.eyJ1IjoidGhlb2xpbm5haWRvbyIsImEiOiJjbDE3djd6ejcwN3FsM2NvNGdmMGFwZzN3In0.cKJRbP39WngD5JNwsphF-g',
-                'id' : 'mapbox.mapbox-streets-v8'
-              },
-              attributionBuilder: (_) {
-                return Text("© OpenStreetMap contributors");
-              },
-            ),
-           /* MarkerLayerOptions(
-              markers: [
-                Marker(
-                  width: 80.0,
-                  height: 80.0,
-                  point: LatLng(37.4217432, -122.0841487),
-                  builder: (ctx) =>
-                     /* Container(
-                        child: FlutterLogo(),
-                      ),*/
-                ),
-              ],
-            ),*/
-          ],
-        )
-      ),
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
     );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
